760c2996a16c71ace3e4ba1872a078f67fe4a214
Brian Hsu <brianhsu.hsu@gmail.com>
2011-12-22 00:16:06 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -16,18 +16,6 @@ var notification = [
     'hime'
 ]
 
-// The following list will control which bulit-in status
-// icon show be hidden.
-
-var removeStatusIcon = [
-    'a11y',         // Accessibility
-    // 'volume',
-    // 'battery',
-    // 'keyboard',
-    // 'bluetooth',
-    // 'network'
-]
-
 
 /******************************************************
  * Don't touch anything below!
@@ -38,26 +26,6 @@ const Panel = imports.ui.panel;
 const Main = imports.ui.main;
 
 
-function hideStatusIcon(name)
-{
-    for (var i = 0; i < Main.panel._rightBox.get_children().length; i++) {
-        if (Main.panel._statusArea[name] == 
-            Main.panel._rightBox.get_children()[i]._delegate) {
-            Main.panel._rightBox.get_children()[i].destroy();
-            break;
-        }
-    }
-
-    Main.panel._statusArea[name] = null;
-}
-
-function restoreStatusIcon(name)
-{
-    let indicator = new Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION[name];
-    Main.panel.addToStatusArea(
-        name, indicator, Panel.STANDARD_STATUS_AREA_ORDER.indexOf(name)
-    );
-}
 
 function removeFromTopBar(wmClass)
 {
@@ -74,15 +42,9 @@ function init() {
 
 function enable() {
 
-
     for (var i = 0; i < notification.length; i++) {
         global.log('Add ' + notification[i] + " to top bar");
         addToTopBar(notification[i]);
-    }
-
-    for (var i = 0; i < removeStatusIcon.length; i++) {
-        global.log('Remove ' + removeStatusIcon[i] + " from top bar");
-        hideStatusIcon(removeStatusIcon[i]);
     }
 
 }
@@ -92,11 +54,6 @@ function disable() {
     for (var i = 0; i < notification.length; i++) {
         global.log('Remove ' + notification[i] + " from top bar");
         removeFromTopBar(notification[i]);
-    }
-
-    for (var i = 0; i < removeStatusIcon.length; i++) {
-        global.log('Restore ' + removeStatusIcon[i] + " to top bar");
-        restoreStatusIcon(removeStatusIcon[i]);
     }
 
 }
