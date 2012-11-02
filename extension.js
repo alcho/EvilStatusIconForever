22a2d0c6da79943f8bb49a7fc7006e20523356e5
mathematicalcoffee <mathematical.coffee@gmail.com>
2012-11-02 14:14:27 +1000

    added 3.6 compatibility

--- extension.js
+++ extension.js
@@ -44,22 +44,26 @@ var removeStatusIcon = [
  * Don't touch anything below!
  *****************************************************/
 
-const StatusIconDispatcher = imports.ui.statusIconDispatcher;
+const Shell = imports.gi.Shell;
+
+const PanelMenu = imports.ui.panelMenu;
 const Panel = imports.ui.panel;
 const Main = imports.ui.main;
+const STANDARD_TRAY_ICON_IMPLEMENTATIONS = imports.ui.notificationDaemon.STANDARD_TRAY_ICON_IMPLEMENTATIONS;
+let trayManager, addedID;
+let statusArea;
 
 /**
  *  Hide built-in status icon.
+ *
+ *  Note: in 3.6 the top-level actor for a status button in
+ *  _rightBox.get_children() doesn't have a ._delegate; its first child does.
+ *  So instead we just remove from the status area directly.
  */
 function hideStatusIcon(name)
 {
-    for (var i = 0; i < Main.panel._rightBox.get_children().length; i++) {
-        if (Main.panel._statusArea[name] == 
-            Main.panel._rightBox.get_children()[i]._delegate) {
-            global.log("HIDE:" + name);
-            Main.panel._rightBox.get_children()[i].hide();
-            break;
-        }
+    if (statusArea[name]) {
+        statusArea[name].actor.hide();
     }
 }
 
@@ -68,30 +72,50 @@ function hideStatusIcon(name)
  */
 function showStatusIcon(name)
 {
-    for (var i = 0; i < Main.panel._rightBox.get_children().length; i++) {
-        if (Main.panel._statusArea[name] == 
-            Main.panel._rightBox.get_children()[i]._delegate) {
-            global.log("SHOW:" + name);
-            Main.panel._rightBox.get_children()[i].show();
-            break;
-        }
+    if (statusArea[name]) {
+        statusArea[name].actor.show();
     }
 }
 
+/** Callback when a tray icon is added to the tray manager.
+ * We make a panel button for the top panel for it. */
+function _onTrayIconAdded(o, icon) {
+    let wmClass = icon.wm_class ? icon.wm_class.toLowerCase() : '';
+    if (notification.indexOf(wmClass) === -1) {
+        // skip it
+        return;
+    }
+
+    icon.height = Panel.PANEL_ICON_SIZE;
+    let buttonBox = new PanelMenu.Button();
+    let box = buttonBox.actor;
+    box.add_actor(icon);
+
+    Main.panel._addToPanelBox(wmClass, buttonBox, 0, Main.panel._rightBox);
+}
+
+// STANDARD_TRAY_ICON_IMPLEMENTATIONS is necessary for 3.6
+// to stop the message tray also making an icon for it.
 function removeFromTopBar(wmClass)
 {
-    delete StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS[wmClass];
+    delete STANDARD_TRAY_ICON_IMPLEMENTATIONS[wmClass];
+    if (statusArea[wmClass]) {
+        statusArea[wmClass].destroy();
+    }
 }
 
 function addToTopBar(wmClass)
 {
-    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS[wmClass] = wmClass;
+    STANDARD_TRAY_ICON_IMPLEMENTATIONS[wmClass] = wmClass;
 }
 
 function init() {
 }
 
 function enable() {
+    statusArea = Main.panel.statusArea;
+    trayManager = Main.notificationDaemon._trayManager;
+    addedID = trayManager.connect('tray-icon-added', _onTrayIconAdded);
 
     for (var i = 0; i < notification.length; i++) {
         global.log('Add ' + notification[i] + " to top bar");
@@ -106,6 +130,8 @@ function enable() {
 }
 
 function disable() {
+    trayManager.disconnect(addedID);
+    addedID = 0;
 
     for (var i = 0; i < notification.length; i++) {
         global.log('Remove ' + notification[i] + " from top bar");
@@ -118,4 +144,3 @@ function disable() {
     }
 
 }
-
